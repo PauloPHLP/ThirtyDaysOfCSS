851acd461ed99a21471939ca9cd2f2485deaf0c7
Paulo <paulo.phlp@outlook.com>
2021-03-24 11:11:06 -0300

    Feature: Add structure for challenges page.

--- src/pages/Challenges/index.tsx
+++ src/pages/Challenges/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
 
 import * as S from './styles';
 
 const Challenges: React.FC = () => {
-  return <S.Container>Challenges</S.Container>;
+  return (
+    <S.Container>
+      <S.Header>
+        <S.GoToChallengesList>
+          <FaArrowLeft
+            size={20}
+            style={{ marginRight: '8px', marginBottom: '-1px' }}
+          />
+          <Link to="/">Challenges list </Link>
+        </S.GoToChallengesList>
+      </S.Header>
+      <S.Square>
+        <S.Challenge>Challenge</S.Challenge>
+        <S.Code>Code</S.Code>
+      </S.Square>
+    </S.Container>
+  );
 };
 
 export default Challenges;
--- src/pages/Challenges/styles.tsx
+++ src/pages/Challenges/styles.tsx
@@ -1,3 +1,130 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 90vh;
+  width: 100vw;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  position: fixed;
+  bottom: 0;
+`;
+
+export const Header = styled.div`
+  position: fixed;
+  top: 0;
+  margin-top: 2rem;
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  max-width: 90%;
+`;
+
+export const GoToChallengesList = styled.div`
+  position: fixed;
+  top: 0;
+  margin-top: 2rem;
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  max-width: 90%;
+  text-align: center;
+  color: #989db8;
+
+  @media (max-width: 260px) {
+    flex-direction: column;
+
+    & > a {
+      display: none;
+    }
+  }
+
+  &:hover {
+    color: #6fd0cf;
+    cursor: pointer;
+  }
+
+  & > a {
+    font-size: 30px;
+    text-decoration: none;
+
+    &:visited {
+      color: #989db8;
+      text-decoration: none;
+    }
+
+    &:hover {
+      color: #6fd0cf;
+      cursor: pointer;
+    }
+  }
+`;
+
+export const Square = styled.div`
+  align-self: center;
+  height: 90%;
+  max-height: 90%;
+  width: 90%;
+  max-width: 90%;
+  border: 3px solid #6fd0cf;
+  border-radius: 16px;
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+
+  @media (max-width: 1200px) {
+    flex-direction: column;
+  }
+`;
+
+const squareItems = styled.div`
+  height: 100%;
+  width: 50%;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+
+  @media (max-width: 1200px) {
+    height: 50%;
+    width: 100%;
+  }
+`;
+
+export const Challenge = styled(squareItems)`
+  border-top-left-radius: 12px;
+  border-bottom-left-radius: 12px;
+  border-right: 3px solid #6fd0cf;
+
+  @media (max-width: 1200px) {
+    border-top-left-radius: 12px;
+    border-top-right-radius: 12px;
+    border-bottom-left-radius: 0px;
+    border-right: none;
+    border-bottom: 3px solid #6fd0cf;
+  }
+`;
+
+export const Code = styled(squareItems)`
+  border-top-right-radius: 12px;
+  border-bottom-right-radius: 12px;
+
+  @media (max-width: 1200px) {
+    border-bottom-left-radius: 12px;
+    border-bottom-right-radius: 12px;
+    border-top-right-radius: 0px;
+  }
+`;
