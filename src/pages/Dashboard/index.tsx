0055b2283fe248bee3055db1b145f18ff01d0302
Paulo Lima <32931744+PauloPHLP@users.noreply.github.com>
2021-06-08 11:37:55 -0300

    Style: Fix Dashboard page CSS structure.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,30 +7,35 @@ import * as S from './styles';
 const Dashboard: React.FC = () => {
   return (
     <S.Container>
-      <S.Square>
-        <S.Social>
-          <S.SocialLink href="https://github.com/PauloPHLP" target="_blank">
-            <FaGithub size={16} style={{ marginRight: '2px' }} /> GitHub
-          </S.SocialLink>
-          &bull;
-          <S.SocialLink
-            href="https://paulophlp.github.io/portfolio/"
-            target="_blank"
-          >
-            <FaTerminal
-              size={16}
-              style={{ marginRight: '4px', marginBottom: '-1px' }}
-            />
-            Paulo Lima
-          </S.SocialLink>
-        </S.Social>
-        <S.Header>Thirty Days Of CSS</S.Header>
-        <S.Description>
-          A thirty days CSS challenge to test your skills
-        </S.Description>
+      <S.InnerContainer>
+        <S.Header>
+          <S.Social>
+            <S.SocialLink href="https://github.com/PauloPHLP" target="_blank">
+              <FaGithub size={16} style={{ marginRight: '2px' }} />
+              <span> GitHub</span>
+            </S.SocialLink>
+            &bull;
+            <S.SocialLink
+              href="https://paulophlp.github.io/portfolio/"
+              target="_blank"
+            >
+              <FaTerminal
+                size={16}
+                style={{ marginRight: '4px', marginBottom: '-1px' }}
+              />
+              <span>Paulo Lima</span>
+            </S.SocialLink>
+          </S.Social>
+
+          <S.Title>Thirty Days Of CSS</S.Title>
+
+          <S.Description>
+            A thirty days CSS challenge to test your skills
+          </S.Description>
+        </S.Header>
 
         <ChallengesList />
-      </S.Square>
+      </S.InnerContainer>
     </S.Container>
   );
 };
--- src/pages/Dashboard/styles.tsx
+++ src/pages/Dashboard/styles.tsx
@@ -3,36 +3,30 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 100vh;
   width: 100vw;
+  padding: 1rem;
+`;
+
+export const InnerContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  border: 1px solid #4a5079;
+  border-radius: 8px;
   display: flex;
-  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+`;
+
+export const Header = styled.header`
+  align-self: flex-start;
+  display: flex;
   flex-direction: column;
   align-items: center;
-  align-content: center;
-  padding: 1rem 1rem;
   text-align: center;
 `;
 
-export const Square = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  align-items: center;
-  padding: 2rem;
-  border: 1px solid #4a5079;
-  border-radius: 8px;
-`;
-
-export const Social = styled.div`
-  margin-bottom: 1rem;
-
-  @media (max-width: 320px) {
-    display: flex;
-    align-items: center;
-    margin-top: -1rem;
-  }
-`;
+export const Social = styled.div``;
 
 export const SocialLink = styled.a`
   font-size: 20px;
@@ -48,36 +42,31 @@ export const SocialLink = styled.a`
     color: #6fd0cf;
     cursor: pointer;
   }
+
+  @media (max-width: 768px) {
+    & > span {
+      display: none;
+    }
+  }
 `;
 
-export const Header = styled.header`
-  font-size: 48px;
+export const Title = styled.div`
+  font-size: 42px;
   color: #ffffff;
   font-weight: 500;
+  margin-top: 0.5rem;
 
-  @media (max-width: 528px) {
-    font-size: 44px;
-  }
-
-  @media (max-width: 450px) {
-    font-size: 28px;
-  }
-
-  @media (max-width: 320px) {
-    font-size: 24px;
-  }
-
-  @media (max-width: 280px) {
-    font-size: 20px;
+  @media (max-width: 768px) {
+    font-size: 30px;
   }
 `;
 
-export const Description = styled.p`
-  margin-top: 1rem;
+export const Description = styled.div`
+  margin-top: 0.3rem;
   color: #989db8;
   font-size: 16px;
 
-  @media (max-width: 280px) {
+  @media (max-width: 768px) {
     font-size: 14px;
   }
 `;
