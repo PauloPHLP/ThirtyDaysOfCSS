085d4bda4ad5867ed68fdb76b32da720066984bc
PauloPHLP <plima.himself@gmail.com>
2021-07-16 07:38:00 -0300

    Feature: Add sixth challenge.

---
+++ src/components/Challenges/Day06/Challenge/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import './styles.css';
+
+const Challenge: React.FC = () => {
+  return (
+    <div className="container-challenge">
+      <div className="lighthing-text">
+        <div className="group-01">
+          <span className="letter">T</span>
+          <span className="letter">H</span>
+          <span className="letter">I</span>
+          <span className="letter">R</span>
+          <span className="letter">T</span>
+          <span className="letter">Y</span>
+        </div>
+        <div className="group-02">
+          <span className="letter">D</span>
+          <span className="letter">A</span>
+          <span className="letter">Y</span>
+          <span className="letter">S</span>
+        </div>
+        <div className="group-03">
+          <span className="letter">O</span>
+          <span className="letter">F</span>
+        </div>
+        <div className="group-04">
+          <span className="letter">C</span>
+          <span className="letter">S</span>
+          <span className="letter">S</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Challenge;
