5c48803a32d39beb509b14945613ba455b4a2c28
PauloPHLP <plima.himself@gmail.com>
2021-07-16 08:22:17 -0300

    Feature: Add eighth challenge.

---
+++ src/components/Challenges/Day08/Challenge/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const Challenge: React.FC = () => {
+  return (
+    <div className="container-challenge">
+      <span className="heart" />
+    </div>
+  );
+};
+
+export default Challenge;
