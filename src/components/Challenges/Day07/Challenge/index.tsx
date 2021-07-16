2bc50e3a0976ac0f91c827007394ff2dc27332c8
PauloPHLP <plima.himself@gmail.com>
2021-07-16 08:06:09 -0300

    Feature: Add seventh challenge.

---
+++ src/components/Challenges/Day07/Challenge/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './styles.css';
+
+const Challenge: React.FC = () => {
+  return (
+    <div className="container-challenge">
+      <div className="squares">
+        <span />
+        <span />
+        <span />
+      </div>
+    </div>
+  );
+};
+
+export default Challenge;
