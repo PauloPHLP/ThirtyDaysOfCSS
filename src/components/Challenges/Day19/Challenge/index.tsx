40de25c9f582d7654e436b984ad2273dfeb1be91
PauloPHLP <plima.himself@gmail.com>
2021-07-21 17:04:53 -0300

    Feature: Add nineteenth challenge.

--- src/components/Challenges/Day19/Challenge/index.tsx
+++ src/components/Challenges/Day19/Challenge/index.tsx
@@ -3,7 +3,30 @@ import React from 'react';
 import './styles.css';
 
 const Challenge: React.FC = () => {
-  return <div className="challenge-container">Day Template</div>;
+  return (
+    <div className="challenge-container">
+      <div className="challenge-19-div">
+        <span className="challenge-19-span" />
+        <span className="challenge-19-span" />
+        <span className="challenge-19-span" />
+      </div>
+      <div className="challenge-19-div">
+        <span className="challenge-19-span" />
+        <span className="challenge-19-span" />
+        <span className="challenge-19-span" />
+      </div>
+      <div className="challenge-19-div">
+        <span className="challenge-19-span" />
+        <span className="challenge-19-span" />
+        <span className="challenge-19-span" />
+      </div>
+      <div className="challenge-19-div">
+        <span className="challenge-19-span" />
+        <span className="challenge-19-span" />
+        <span className="challenge-19-span" />
+      </div>
+    </div>
+  );
 };
 
 export default Challenge;
