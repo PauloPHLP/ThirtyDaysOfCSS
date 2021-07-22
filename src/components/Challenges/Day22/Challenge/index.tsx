fd726c2f12a974936da5c1295cd3a43736a21352
PauloPHLP <plima.himself@gmail.com>
2021-07-22 16:47:41 -0300

    Feature: Add twenty-second challenge.

--- src/components/Challenges/Day22/Challenge/index.tsx
+++ src/components/Challenges/Day22/Challenge/index.tsx
@@ -3,7 +3,24 @@ import React from 'react';
 import './styles.css';
 
 const Challenge: React.FC = () => {
-  return <div className="challenge-container">Day Template</div>;
+  return (
+    <div className="challenge-container">
+      <ul className="challenge-22-ul">
+        <li className="challenge-22-li">
+          <i className="fas fa-heart challenge-22-i" />
+        </li>
+        <li className="challenge-22-li">
+          <i className="fas fa-glass-martini challenge-22-i" />
+        </li>
+        <li className="challenge-22-li">
+          <i className="fas fa-globe challenge-22-i" />
+        </li>
+        <li className="challenge-22-li">
+          <i className="fas fa-gift challenge-22-i" />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Challenge;
