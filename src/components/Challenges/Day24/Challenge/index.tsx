0b9539d339a1041338f9b285cee7ad8bea0dfa72
PauloPHLP <plima.himself@gmail.com>
2021-07-22 17:11:46 -0300

    Feature: Add twenty-fifth challenge.

--- src/components/Challenges/Day24/Challenge/index.tsx
+++ src/components/Challenges/Day24/Challenge/index.tsx
@@ -3,7 +3,16 @@ import React from 'react';
 import './styles.css';
 
 const Challenge: React.FC = () => {
-  return <div className="challenge-container">Day Template</div>;
+  return (
+    <div className="challenge-container">
+      <div className="challenge-24-container">
+        <div className="challenge-24-square" />
+        <div className="challenge-24-square" />
+        <div className="challenge-24-square" />
+        <div className="challenge-24-square" />
+      </div>
+    </div>
+  );
 };
 
 export default Challenge;
