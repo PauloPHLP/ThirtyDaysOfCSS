7537a160d2897d451c06bd11861786fe4c1f34b4
PauloPHLP <plima.himself@gmail.com>
2021-07-23 15:16:01 -0300

    Feature: Add thirtieth challenge.

--- src/components/Challenges/Day30/Challenge/index.tsx
+++ src/components/Challenges/Day30/Challenge/index.tsx
@@ -3,7 +3,68 @@ import React from 'react';
 import './styles.css';
 
 const Challenge: React.FC = () => {
-  return <div className="challenge-container">Day Template</div>;
+  return (
+    <div className="challenge-container">
+      <div className="firewors-container">
+        <div className="firework">
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+        </div>
+        <div className="firework" id="firework1">
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+        </div>
+        <div className="firework" id="firework2">
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+        </div>
+        <div className="firework" id="firework3">
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+          <div className="explosion" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Challenge;
