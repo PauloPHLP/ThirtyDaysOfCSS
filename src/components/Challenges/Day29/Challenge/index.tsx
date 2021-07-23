45d04e834f2ad914252f3fe1c74d9d822bad0807
PauloPHLP <plima.himself@gmail.com>
2021-07-23 15:09:28 -0300

    Feature: Add twenty-nineth challenge.

--- src/components/Challenges/Day29/Challenge/index.tsx
+++ src/components/Challenges/Day29/Challenge/index.tsx
@@ -1,9 +1,35 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
 import './styles.css';
 
 const Challenge: React.FC = () => {
-  return <div className="challenge-container">Day Template</div>;
+  return (
+    <div className="challenge-container">
+      <div className="challenge-29-container">
+        <div className="navbar">
+          <label htmlFor="toggle" className="challenge-29-label">
+            &#9776;
+          </label>
+          <input type="checkbox" id="toggle" className="challenge-29-toggle" />
+          <div className="menu">
+            <a className="challenge-29-a" href="/challenges/day-29">
+              Home
+            </a>
+            <a className="challenge-29-a" href="/challenges/day-29">
+              Services
+            </a>
+            <a className="challenge-29-a" href="/challenges/day-29">
+              About
+            </a>
+            <a className="challenge-29-a" href="/challenges/day-29">
+              <span>Log Off</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Challenge;
