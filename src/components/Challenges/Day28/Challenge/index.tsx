0923ecc4777ed9725b438161366aa1a8dee13734
PauloPHLP <plima.himself@gmail.com>
2021-07-23 14:40:57 -0300

    Feature: Add twenty-eigth challenge.

--- src/components/Challenges/Day28/Challenge/index.tsx
+++ src/components/Challenges/Day28/Challenge/index.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import './styles.css';
 
 const Challenge: React.FC = () => {
-  return <div className="challenge-container">Day Template</div>;
+  return (
+    <div className="challenge-container">
+      <div className="background">Challenge 28</div>
+    </div>
+  );
 };
 
 export default Challenge;
--- src/components/Challenges/Day28/Code/index.tsx
+++ src/components/Challenges/Day28/Code/index.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
 import { CopyBlock, dracula } from 'react-code-blocks';
 
-const CSS_CODE = ``;
+const CSS_CODE = `/* Default style for all challenges containers. */
+.challenge-container {
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  /* Overridden for this component */
+  width: 100%;
+  justify-content: center;
+  overflow: hidden;
+}
+
+.background {
+  height: 100%;
+  width: 100%;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #55acee;
+  font-size: 48px;
+  text-align: center;
+  animation: change-colors 5s linear infinite;
+}
+
+@keyframes change-colors {
+  0% {
+    filter: hue-rotate(0deg);
+  }
+
+  100% {
+    filter: hue-rotate(360deg);
+  }
+}
+`;
 
 const Code: React.FC = () => {
   return (
