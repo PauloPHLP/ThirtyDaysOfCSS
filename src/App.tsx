a3c67650c68ed11fb98448044a11988424015fc6
PauloPHLP <plima.himself@gmail.com>
2021-07-28 17:24:55 -0300

    Chore: Fix route basename.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 const App: React.FC = () => (
-  <Router>
+  <Router basename="/ThirtyDaysOfCSS">
     <Routes />
     <GlobalStyle />
   </Router>
